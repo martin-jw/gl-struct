f0107573349687f6fc649a6d457afc31c67df0ca
Martin <martin.jirlow@gmail.com>
2020-02-24 10:11:10 +0100

    Add actual build script and struct generation

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,31 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+mod bindings {
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+}
+
+pub use bindings::*;
+use std::ops::Deref;
+use std::rc::Rc;
+
+#[derive(Clone)]
+pub struct Gl {
+    inner: Rc<crate::bindings::Gl>,
+}
+
+impl Gl {
+    pub fn load_with<F>(loadfn: F) -> Gl
+    where
+        F: FnMut(&'static str) -> *const crate::types::GLvoid,
+    {
+        Gl {
+            inner: Rc::new(crate::bindings::Gl::load_with(loadfn)),
+        }
+    }
+}
+
+impl Deref for Gl {
+    type Target = crate::bindings::Gl;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
     }
 }
